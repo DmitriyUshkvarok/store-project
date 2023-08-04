2fd7ced6b28c59dc3b3bfab4b2819d30fa7de714
OPaslavskiy <bankir.op@gmail.com>
2023-08-04 16:11:56 +0300

    add simplelightbox

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,40 +1,38 @@
-// 'use client';
-// import { catalog } from './dataCatalogList';
-// import Link from 'next/link';
-// import Image from 'next/image';
-// import { useRouter } from 'next/navigation';
-
-export const photos = [
-  'photoForGalerryHARD/211.jpg',
-  'photoForGalerryHARD/398.jpg',
-  'photoForGalerryHARD/78.jpg',
-  'photoForGalerryHARD/211.jpg',
-  'photoForGalerryHARD/398.jpg',
-  'photoForGalerryHARD/78.jpg',
-  'photoForGalerryHARD/211.jpg',
-  'photoForGalerryHARD/398.jpg',
-  'photoForGalerryHARD/78.jpg',
-  'photoForGalerryHARD/78.jpg',
-  'photoForGalerryHARD/211.jpg',
-  'photoForGalerryHARD/398.jpg',
-  'photoForGalerryHARD/78.jpg',
-  'photoForGalerryHARD/78.jpg',
-  'photoForGalerryHARD/211.jpg',
-  'photoForGalerryHARD/398.jpg',
-  'photoForGalerryHARD/78.jpg',
-];
-
-console.log(photos);
+'use client';
+import React, { useEffect } from 'react';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const GalleryList = () => {
-  console.log(photos);
-  return (
-    <div>
-      {photos.map((photo, index) => (
-        <img src={photo} alt="2222" key={index} width="330" height="206" />
-      ))}
+  const photos = [
+    'photoForGalerryHARD/211.jpg',
+    'photoForGalerryHARD/398.jpg',
+    'photoForGalerryHARD/78.jpg',
+    'photoForGalerryHARD/431.jpg',
+    'photoForGalerryHARD/4151132.jpg',
+  ];
 
-      <p>111</p>
+  useEffect(() => {
+    const lightbox = new SimpleLightbox('.gallery a', {
+      captionDelay: 250,
+      disableRightClick: true,
+    });
+
+    return () => lightbox.destroy();
+  }, []);
+
+  return (
+    <div className="gallery">
+      {photos.map((photo, index) => (
+        <a href={photo} key={index}>
+          <img
+            src={photo}
+            alt={`Image ${index + 1}`}
+            width="330"
+            height="206"
+          />
+        </a>
+      ))}
     </div>
   );
 };
