1c74f212976c8d214be7036d17dfdb1c62e5969e
OPaslavskiy <bankir.op@gmail.com>
2023-08-04 17:17:04 +0300

    add pagination

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,38 +1,90 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import Image from 'next/image';
 
 const GalleryList = () => {
   const photos = [
-    'photoForGalerryHARD/211.jpg',
-    'photoForGalerryHARD/398.jpg',
-    'photoForGalerryHARD/78.jpg',
-    'photoForGalerryHARD/431.jpg',
-    'photoForGalerryHARD/4151132.jpg',
+    '/photoForGalerryHARD/211.jpg',
+    '/photoForGalerryHARD/398.jpg',
+    '/photoForGalerryHARD/78.jpg',
+    '/photoForGalerryHARD/431.jpg',
+    '/photoForGalerryHARD/4151132.jpg',
   ];
 
-  useEffect(() => {
-    const lightbox = new SimpleLightbox('.gallery a', {
-      captionDelay: 250,
-      disableRightClick: true,
-    });
+  const itemsPerPage = 3; // Кількість фото на одній сторінці
 
-    return () => lightbox.destroy();
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPhotos = photos.length;
+  const totalPages = Math.ceil(totalPhotos / itemsPerPage);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const lightbox = new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+        disableRightClick: true,
+        showCounter: false,
+      });
+
+      lightbox.on('shown.simplelightbox', () => {
+        document.body.classList.add('body-lock');
+      });
+
+      lightbox.on('close.simplelightbox', () => {
+        document.body.classList.remove('body-lock');
+      });
+
+      return () => {
+        lightbox.destroy();
+      };
+    }
   }, []);
 
+  // Перехід до попередньої сторінки
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+    }
+  };
+
+  // Перехід до наступної сторінки
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+
+  // Фотографії для поточної сторінки
+  const currentPhotos = photos.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
   return (
-    <div className="gallery">
-      {photos.map((photo, index) => (
-        <a href={photo} key={index}>
-          <img
-            src={photo}
-            alt={`Image ${index + 1}`}
-            width="330"
-            height="206"
-          />
-        </a>
-      ))}
+    <div>
+      <div className="gallery">
+        {currentPhotos.map((photo, index) => (
+          <a href={photo} key={index}>
+            <Image
+              src={photo}
+              alt={`Image ${index + 1}`}
+              width="330"
+              height="206"
+            />
+          </a>
+        ))}
+      </div>
+
+      <div className="pagination">
+        <button onClick={handlePrevPage} disabled={currentPage === 1}>
+          Prev
+        </button>
+        <span>{`${currentPage} / ${totalPages}`}</span>
+        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
