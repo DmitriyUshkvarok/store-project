248e44e1f0aaa1cd22a07ef1a27dd6d816b1048c
vitalik.nozhenko@gmail.com <vitalik.nozhenko@gmail.com>
2023-08-12 20:53:34 +0300

    admin-galery

---
+++ src/components/ProductManagementForGallery/ProductManagementForGallery.jsx
@@ -0,0 +1,115 @@
+// 'use client';
+
+// const ProductManagementForGallery = () => {
+//   return <div>ProductManagementForGallery</div>;
+// };
+
+// export default ProductManagementForGallery;
+// //
+
+'use client';
+import React, { useState, useEffect } from 'react';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import ButtonWhiteAndBlack from '../ButtonWhiteAndBlack/ButtonWhiteAndBlack';
+import Spinner from '../SpinerOferta/SpinerOferta';
+import { useGetGalleryQuery } from '@/src/redux/galleryApi/galleryApi';
+import {
+  Box,
+  Gallery,
+  GalleryBox,
+  PageNumber,
+  PaginationBox,
+  Picture,
+  Title,
+} from './ProductManagementForGallery.styled';
+
+const ProductManagementForGallery = () => {
+  const { data, isError, isLoading } = useGetGalleryQuery();
+  const itemsPerPage = 20;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [currentPhotos, setCurrentPhotos] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+      setTotalPages(Math.ceil(data.length / itemsPerPage));
+      setCurrentPhotos(
+        data.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+      );
+    }
+  }, [data, currentPage]);
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const lightbox = new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+        disableRightClick: true,
+        showCounter: false,
+        scrollZoom: false,
+      });
+
+      lightbox.on('shown.simplelightbox', () => {
+        document.body.classList.add('body-lock');
+      });
+
+      lightbox.on('close.simplelightbox', () => {
+        document.body.classList.remove('body-lock');
+      });
+
+      return () => {
+        lightbox.destroy();
+      };
+    }
+  }, [currentPhotos]);
+
+  return (
+    <GalleryBox>
+      <Box>
+        <Gallery className="gallery">
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            currentPhotos.map(({ url, _id }) => (
+              <li key={_id}>
+                <a href={url}>
+                  <Picture src={url} alt={`Image`} width="330" height="206" />
+                </a>
+              </li>
+            ))
+          )}
+        </Gallery>
+      </Box>
+
+      <PaginationBox className="pagination">
+        <ButtonWhiteAndBlack
+          onClick={handlePrevPage}
+          disabled={currentPage === 1}
+        >
+          Попередні фото
+        </ButtonWhiteAndBlack>
+        <PageNumber>{`${currentPage} / ${totalPages}`}</PageNumber>
+        <ButtonWhiteAndBlack
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+        >
+          Наступні фото
+        </ButtonWhiteAndBlack>
+      </PaginationBox>
+    </GalleryBox>
+  );
+};
+
+export default ProductManagementForGallery;
---
+++ src/components/ProductManagementForGallery/ProductManagementForGallery.styled.jsx
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+import Image from 'next/image';
+
+export const GalleryBox = styled.div`
+  padding-left: 30px;
+  padding-right: 30px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const Gallery = styled.ul`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 5px;
+
+  @media screen and (min-width: 1060px) {
+    justify-content: start;
+  }
+`;
+
+export const Title = styled.h1`
+  margin-top: 30px;
+`;
+
+export const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex-direction: column;
+  max-width: 1000px;
+  margin: 0 auto;
+
+  @media screen and (min-width: 1060px) {
+    justify-content: start;
+  }
+`;
+
+export const PaginationBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 34px;
+  margin-bottom: 34px;
+
+  @media screen and (min-width: 700px) {
+    flex-direction: row;
+  }
+`;
+
+export const PageNumber = styled.span`
+  margin-top: 10px;
+  margin-bottom: 10px;
+  font-weight: 800;
+
+  @media screen and (min-width: 700px) {
+    margin: 0 15px 0 15px;
+  }
+`;
+
+export const Picture = styled(Image)`
+  object-fit: cover;
+  transition: transform 0.25s;
+  overflow: hidden;
+  max-height: 110px;
+  width: 160px;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
