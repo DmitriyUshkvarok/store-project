bdbade3d6fa9bbcccf42df297cf93063cab3e589
OPaslavskiy <bankir.op@gmail.com>
2023-08-17 20:45:08 +0300

    1

--- src/components/BtnBuy/BynBuy.styled.jsx
+++ src/components/BtnBuy/BynBuy.styled.jsx
@@ -3,10 +3,8 @@ import styled from 'styled-components';
 export const Btn = styled.button`
   cursor: pointer;
   color: #000000;
-  /* display: flex; */
   align-items: center;
   gap: 20px;
-  font-family: gotham;
   font-size: 14px;
   font-weight: bold;
   height: 42px;
