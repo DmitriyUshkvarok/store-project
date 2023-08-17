b4ef1c0e0949d6b7164069782678b4dc57442108
OPaslavskiy <bankir.op@gmail.com>
2023-08-17 20:34:25 +0300

    style for product

--- src/components/BtnBuy/BynBuy.styled.jsx
+++ src/components/BtnBuy/BynBuy.styled.jsx
@@ -1,32 +1,28 @@
 import styled from 'styled-components';
 
 export const Btn = styled.button`
-  color: #000000;
-  background: transparent;
-  justify-content: center;
-  display: flex;
-
-  align-items: center;
-  gap: 2px;
-  font-size: 23px;
-
-  letter-spacing: 3px;
-
-  text-transform: uppercase;
-
-  border: 2px solid black;
-  box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
   cursor: pointer;
-  padding-left: 10px;
-  padding-right: 10px;
-  border-radius: 4px;
-  transition: background-color 0.4s;
+  color: #000000;
+  /* display: flex; */
+  align-items: center;
+  gap: 20px;
+  font-family: gotham;
+  font-size: 14px;
+  font-weight: bold;
+  height: 42px;
+  letter-spacing: 6px;
+  line-height: 42px;
+  text-transform: uppercase;
+  border: 2px solid;
+  padding-left: 15px;
+  padding-right: 15px;
+
+  &:hover {
+    outline: none;
+    opacity: 0.7;
+  }
 
   &:disabled {
     opacity: 0.3;
-  }
-
-  &:hover {
-    background-color: rgba(0, 0, 0, 0.1);
   }
 `;
--- src/components/ProductDetail/ProductDetail.styled.js
+++ src/components/ProductDetail/ProductDetail.styled.js
@@ -3,19 +3,43 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 export const ProductDetailSection = styled.section`
-  width: 100vw;
-  min-height: 100vh;
-  background-color: #80808029;
-  padding-top: 20px;
-  padding-bottom: 20px;
+  padding-left: 30px;
+  padding-right: 30px;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 export const LinkPanel = styled.div`
-  padding: 10px;
-  margin-bottom: 20px;
+  margin-top: 30px;
+`;
+
+export const Product = styled.div`
   display: flex;
-  gap: 10px;
+  justify-content: center;
   flex-wrap: wrap;
+  gap: 5px;
+
+  padding-top: 30px;
+  border-top: 4px solid gray;
+
+  margin-top: 30px;
+
+  @media screen and (min-width: 1060px) {
+    justify-content: start;
+  }
+`;
+
+export const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex-direction: column;
+  max-width: 1000px;
+  margin: 0 auto;
+
+  @media screen and (min-width: 1060px) {
+    justify-content: start;
+  }
 `;
 
 export const StyleLinkDetail = styled(Link)`
@@ -30,8 +54,10 @@ export const CurrentLink = styled.span`
 
 export const ProductDetailInfoBlock = styled.div`
   display: flex;
-  padding: 20px;
+  margin-left: auto;
+  margin-right: auto;
   border-radius: 4px;
+  margin-bottom: 20px;
 
   @media screen and (max-width: 580px) {
     flex-wrap: wrap;
@@ -44,6 +70,8 @@ export const ProductDetailInfoBlock = styled.div`
 export const StyleImage = styled(Image)`
   margin-bottom: 10px;
   object-fit: cover;
+  width: 100%;
+  height: auto;
 `;
 
 export const ProductСharacterization = styled.p`
@@ -54,25 +82,30 @@ export const ProductСharacterization = styled.p`
 `;
 
 export const ProductDescription = styled.p`
-  text-align: center;
-  color: black;
-  font-weight: bold;
+  font-weight: 400;
+  text-align: justify;
 `;
 
 export const ProductWeight = styled.div`
   color: #222;
-  font-weight: bold;
+  font-weight: 500;
   margin-bottom: 10px;
 `;
 
 export const CategorySpan = styled.span`
-  color: lightcoral;
-  font-weight: normal;
+  font-size: 20px;
+  font-weight: 300;
   margin-left: 10px;
 `;
 
+export const CategorySpanPdf = styled.span`
+  font-size: 15px;
+  font-weight: 300;
+  margin-left: 5px;
+`;
+
 export const ProductBlockLeft = styled.div`
-  padding: 20px;
+  /* padding: 20px; */
   width: 50%;
   border-radius: 4px;
 
@@ -90,7 +123,7 @@ export const ImageBlock = styled.div`
 `;
 
 export const ProductFullName = styled.p`
-  color: #222;
+  font-size: 25px;
   font-weight: bold;
   text-transform: uppercase;
   text-align: center;
@@ -98,87 +131,88 @@ export const ProductFullName = styled.p`
 `;
 
 export const ProductInfoName = styled.p`
-  color: #222;
-  font-weight: bold;
-  margin-bottom: 7px;
+  font-size: 20px;
+  font-weight: 300;
+  margin-bottom: 10px;
 `;
 
 export const ProductName = styled.p`
-  color: #222;
-  font-weight: bold;
-  margin-bottom: 7px;
+  font-size: 20px;
+  font-weight: 300;
+  margin-bottom: 10px;
+  border-bottom: 1px solid grey;
+  padding-bottom: 10px;
 `;
 
 export const ProductPrice = styled.p`
   color: #222;
-  text-transform: uppercase;
-  text-align: center;
-  font-size: 23px;
+  font-size: 20px;
+  font-weight: bold;
+  color: black;
+  margin-bottom: 10px;
+  border-top: 1px solid grey;
+  padding-top: 10px;
 `;
 
 export const SpanPrice = styled.span`
-  color: gray;
-  font-weight: bold;
-  margin-left: 5px;
+  font-size: 20px;
+  font-weight: 300;
+  margin-left: 10px;
 `;
 
 export const ProductBrand = styled.p`
   color: #222;
-  font-weight: bold;
+  font-weight: 500;
   margin-bottom: 10px;
 `;
 
 export const ProductColor = styled.p`
   color: #222;
-  font-weight: bold;
+  font-weight: 500;
   margin-bottom: 10px;
 `;
 
 export const ProductPackingType = styled.p`
-  font-weight: bold;
+  font-weight: 500;
   color: black;
   margin-bottom: 10px;
 `;
 
 export const ProductCountry = styled.div`
-  font-weight: bold;
+  font-weight: 500;
   color: black;
   margin-bottom: 10px;
 `;
 
 export const ProductType = styled.p`
-  font-weight: bold;
+  font-weight: 500;
   color: black;
   margin-bottom: 10px;
 `;
 
 export const ProductFormula = styled.p`
-  font-weight: bold;
+  font-weight: 500;
   color: black;
   margin-bottom: 10px;
 `;
 
 export const ProductDensity = styled.p`
-  font-weight: bold;
+  font-weight: 500;
   color: black;
   margin-bottom: 10px;
 `;
 
 export const ProductPdf = styled.div`
-  font-weight: bold;
+  font-weight: 500;
   color: black;
-  margin-bottom: 30px;
-
-  @media screen and (max-width: 380px) {
-    text-align: center;
-  }
+  margin-bottom: 10px;
 `;
 
 export const ProductBlockRight = styled.div`
   display: flex;
   flex-direction: column;
   width: 50%;
-  padding: 20px;
+  /* padding: 20px; */
   border-radius: 4px;
 
   @media screen and (max-width: 580px) {
@@ -186,28 +220,23 @@ export const ProductBlockRight = styled.div`
   }
 `;
 
+export const Boxer = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+`;
+
 export const CounterAndBtnWrapper = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
+  gap: 15px;
   margin-top: 20px;
-  gap: 20px;
-
-  @media screen and (max-width: 1070px) {
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: flex-start;
-    flex-direction: column;
-    gap: 10px;
-  }
+  margin-bottom: 20px;
 `;
 
 export const CounterWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.3);
   border-radius: 10px;
   max-width: 150px;
   padding-left: 10px;
