fd90c74014170ef13159a5d65e6557e75c42ed96
OPaslavskiy <bankir.op@gmail.com>
2023-08-17 20:57:21 +0300

    d

--- src/components/BtnBuy/BynBuy.styled.jsx
+++ src/components/BtnBuy/BynBuy.styled.jsx
@@ -3,18 +3,21 @@ import styled from 'styled-components';
 export const Btn = styled.button`
   cursor: pointer;
   color: #000000;
+
+  display: flex;
   align-items: center;
+  justify-content: center;
   gap: 20px;
   font-size: 14px;
-  font-weight: bold;
+  font-weight: 400;
   height: 42px;
-  letter-spacing: 6px;
+  padding-left: 10px;
+  padding-right: 10px;
   line-height: 42px;
-  text-transform: uppercase;
+  min-width: 130px;
   border: 2px solid;
-  padding-left: 15px;
-  padding-right: 15px;
 
+  margin: 0 auto;
   &:hover {
     outline: none;
     opacity: 0.7;
