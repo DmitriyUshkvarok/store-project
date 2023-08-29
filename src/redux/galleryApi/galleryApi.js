a154e1b84aa45c87feb8854b2db80eaa594cb76d
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2023-08-29 14:10:10 +0300

    test requst gallery

--- src/redux/galleryApi/galleryApi.js
+++ src/redux/galleryApi/galleryApi.js
@@ -1,9 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+let protocol = '';
+let host = '';
+
+if (typeof window !== 'undefined') {
+  protocol = window.location.protocol;
+  host = window.location.hostname;
+}
+
 export const galleryApi = createApi({
   reducerPath: 'galleryApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://paints.onrender.com',
+    baseUrl: `${protocol}/${host}:3001/api`,
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) {
